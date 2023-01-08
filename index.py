5e99c5558fc1d8f53431f13c95abab379ac253b7
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-08 15:44:58 -0700

    test-file-reduction

--- index.py
+++ index.py
@@ -1,67 +1,55 @@
-from flask import Flask, request, current_app, json
-import requests
+from flask import Flask, request, current_app
 
+import requests
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
-@app.route('/')
-def index():
-    return '<h1>Youve reached the hodlwatch indicator service, we arent in right now</h1>'
-
-
-@app.route('/asset-pair-data')
-def assetPairData():
+@app.route('/btc-macro-data')
+def macroData():
     SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
-    name = request.args.get('name', None)
     sentKey = request.args.get('api_key', None)
-    print(sentKey, 'IN ASSET PAIR DATA')
+
+    print(sentKey, 'IN BTC MACRO DATA')
 
     if not sentKey or sentKey != SERVER_KEY:
         return "Not authorized", 403
 
-    pairData = requests.get(
-        'https://min-api.cryptocompare.com/data/top/volumes?tsym={}'.format(name))
-
-    result = app.response_class(
-        response=json.dumps(pairData.json()),
-        status=200,
-        mimetype='application/json'
-    )
-
-    return result
-
-
-@app.route('/asset-price-data')
-def asset():
-    SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
-    name = request.args.get('name', None)
-    time = request.args.get('time', None)
-    sentKey = request.args.get('api_key', None)
-
-    print(sentKey)
-
-    if not sentKey or sentKey != SERVER_KEY:
-        return "Not authorized", 403
+    name = 'BTC'
+    time = 1827
 
     priceData = requests.get(
-        'https://min-api.cryptocompare.com/data/v2/histoday?fsym={}&tsym=USD&limit={}'.format(name, time))
+        'https://min-api.cryptocompare.com/data/v2/histoday?fsym={}&tsym=USD&limit={}'.format(name, time)).json()
 
-    if name == 'BTC' or name == 'ETH':
-        CRYPTO_KEY = current_app.config["CRYPTO_COMPARE_KEY"]
+    df = pd.DataFrame(priceData['Data']['Data'])
+    df['totalvolume'] = df['volumefrom'] + df['volumeto']
 
-        print(CRYPTO_KEY)
+    volume = df['totalvolume']
+    price = df['close']
+    time = 365
 
-        blockchainData = requests.get(
-            'https://min-api.cryptocompare.com/data/blockchain/histo/day?fsym={}&limit={}&api_key={}'.format(name, time, CRYPTO_KEY))
+    df = df.assign(VWAP=((volume * price).cumsum() / volume.cumsum()).ffill())
+    df = df.assign(TWAP=(price.cumsum() / len(price)).ffill())
+    returns = (df.close - df.close.shift(1))/df.close.shift(1)
 
-    obs = [priceData.json(), blockchainData.json()]
+    df['returns'] = returns.fillna(0)
+
+    rolling = df['returns'].rolling(window=time)
+    rolling_sharpe_s = np.sqrt(time) * (rolling.mean() / rolling.std())
+
+    df['rolling_sharpe'] = rolling_sharpe_s
 
     result = app.response_class(
-        response=json.dumps(obs),
+        response=json.dumps(df.to_json(orient="records")),
         status=200,
         mimetype='application/json'
     )
 
     return result
+
+
+if __name__ == '__main__':
+    app.run()
