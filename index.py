722a7cad1b1b16a90310b5ed347f24c161f08375
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-08 12:37:19 -0700

    update assetPairData to service

--- index.py
+++ index.py
@@ -13,11 +13,31 @@ def index():
     return '<h1>Youve reached the hodlwatch indicator service, we arent in right now</h1>'
 
 
-@app.route('/asset')
-def asset():
-
+@app.route('/asset-pair-data')
+def assetPairData():
     SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
+    name = request.args.get('name', None)
+    sentKey = request.args.get('api_key', None)
+    print(sentKey, 'IN ASSET PAIR DATA')
 
+    if not sentKey or sentKey != SERVER_KEY:
+        return "Not authorized", 403
+
+    pairData = requests.get(
+        'https://min-api.cryptocompare.com/data/top/volumes?tsym={}'.format(name))
+
+    result = app.response_class(
+        response=json.dumps(pairData.json()),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return result
+
+
+@app.route('/asset-price-data')
+def asset():
+    SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
     name = request.args.get('name', None)
     time = request.args.get('time', None)
     sentKey = request.args.get('api_key', None)
