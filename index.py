6236a114a719c23eb09e277ba2e4738aeae3570d
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-08 15:32:06 -0700

    split/index/macro

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
 from flask import Flask, request, current_app, json
-from pycoingecko import CoinGeckoAPI
 import requests
-import pandas as pd
-import numpy as np
 
-cg = CoinGeckoAPI()
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
@@ -68,50 +65,3 @@ def asset():
     )
 
     return result
-
-
-@app.route('/btc-macro-data')
-def macroData():
-    SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
-    sentKey = request.args.get('api_key', None)
-
-    print(sentKey, 'IN BTC MACRO DATA')
-
-    if not sentKey or sentKey != SERVER_KEY:
-        return "Not authorized", 403
-
-    name = 'BTC'
-    time = 1827
-
-    priceData = requests.get(
-        'https://min-api.cryptocompare.com/data/v2/histoday?fsym={}&tsym=USD&limit={}'.format(name, time)).json()
-
-    df = pd.DataFrame(priceData['Data']['Data'])
-    df['totalvolume'] = df['volumefrom'] + df['volumeto']
-
-    volume = df['totalvolume']
-    price = df['close']
-    time = 365
-
-    df = df.assign(VWAP=((volume * price).cumsum() / volume.cumsum()).ffill())
-    df = df.assign(TWAP=(price.cumsum() / len(price)).ffill())
-    returns = (df.close - df.close.shift(1))/df.close.shift(1)
-
-    df['returns'] = returns.fillna(0)
-
-    rolling = df['returns'].rolling(window=time)
-    rolling_sharpe_s = np.sqrt(time) * (rolling.mean() / rolling.std())
-
-    df['rolling_sharpe'] = rolling_sharpe_s
-
-    result = app.response_class(
-        response=json.dumps(df.to_json(orient="records")),
-        status=200,
-        mimetype='application/json'
-    )
-
-    return result
-
-
-if __name__ == '__main__':
-    app.run()
