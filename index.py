193aa6cd826effbcef41b0e0e4aa447606512e4d
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-09 16:02:07 -0700

    reduce file size

--- index.py
+++ index.py
@@ -13,34 +13,20 @@ def macroData():
     SERVER_KEY = current_app.config["INIDCATOR_SERVER_KEY"]
     sentKey = request.args.get('api_key', None)
 
-    print(sentKey, 'IN BTC MACRO DATA')
-
     if not sentKey or sentKey != SERVER_KEY:
         return "Not authorized", 403
 
-    name = 'BTC'
-    time = 1827
-
     priceData = requests.get(
-        'https://min-api.cryptocompare.com/data/v2/histoday?fsym={}&tsym=USD&limit={}'.format(name, time)).json()
+        'https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=1827').json()
 
     df = pd.DataFrame(priceData['Data']['Data'])
-    df['totalvolume'] = df['volumefrom'] + df['volumeto']
 
-    volume = df['totalvolume']
-    price = df['close']
-    time = 365
 
-    df = df.assign(VWAP=((volume * price).cumsum() / volume.cumsum()).ffill())
-    df = df.assign(TWAP=(price.cumsum() / len(price)).ffill())
-    returns = (df.close - df.close.shift(1))/df.close.shift(1)
+    df = df.assign(VWAP=(((df['volumefrom'] + df['volumeto']) * df['close']).cumsum() / (df['volumefrom'] + df['volumeto']).cumsum()).ffill())
+    df = df.assign(TWAP=(df['close'].cumsum() / len(df['close'])).ffill())
 
-    df['returns'] = returns.fillna(0)
-
-    rolling = df['returns'].rolling(window=time)
-    rolling_sharpe_s = np.sqrt(time) * (rolling.mean() / rolling.std())
-
-    df['rolling_sharpe'] = rolling_sharpe_s
+    rolling = ((df.close - df.close.shift(1))/df.close.shift(1)).fillna(0).rolling(window=365)
+    df['rolling_sharpe'] = np.sqrt(365) * (rolling.mean() / rolling.std())
 
     result = app.response_class(
         response=json.dumps(df.to_json(orient="records")),
